68d44e81ce206a26e891807a707284d22b6ef928
Macner001 <104127090+Macner001@users.noreply.github.com>
2024-10-19 23:45:51 +0530

    modified:   Server.py

---
+++ Server.py
@@ -0,0 +1,22 @@
+from crypt import methods
+from flask import Flask,request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login',methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    if username == 'admin' and password == 'password':
+        return 'Login Successfull'
+    else:
+        return 'Invalid Credentials'
+    
+
+
+if __name__=="__main__":
+    app.run(debug=True)
